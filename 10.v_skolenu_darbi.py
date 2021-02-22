1480b64d3c2130ddb9cbf063aae4f35a44ea74ad
Grencione <76771267+Grencione@users.noreply.github.com>
2021-02-23 00:11:03 +0200

    Add files via upload

---
+++ 10.v_skolenu_darbi.py
@@ -0,0 +1,28 @@
+#Tatjana Grencione
+
+#1)Andersone Linda Elva
+
+#2)Apsîtis Edvards
+
+#3)Èeròevièa Letîcija Lîna
+
+#4)Drozdova Miðelle
+
+#5)Karlinskis Renârs
+
+#6)Lukjanskis Miks Patriks
+
+#7)Lukðs Lauris
+
+#8)Mâlkalns Kristaps
+
+#9)Ruòìis Kristaps
+
+#10)Segliòð Jçkabs
+
+#11)Serþante Agate
+
+#12)Voiteðonoks Aleksandrs
+
+#13)Zvaigzne Gustavs Henrijs
+
