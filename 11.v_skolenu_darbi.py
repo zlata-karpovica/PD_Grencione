169b6e4e81b6b4b0d654e469529066b8d4788bd5
Grencione <76771267+Grencione@users.noreply.github.com>
2021-02-23 22:52:12 +0200

    Add files via upload

---
+++ 11.v_skolenu_darbi.py
@@ -0,0 +1,28 @@
+#Tatjana Grencione
+
+#1)Auniòð Rûdolfs
+
+#2)Âdams Aleksis Henrijs
+
+#3)Dorbe Henrijs
+
+#4)Dunajevs Mârtiòð
+
+#5)Gulbis Kristaps
+
+#6)Kalniòa Agnese
+
+#7) Knope Elizabete
+
+#8) Laizâns Ralfs Adrians
+
+#9)Neija Niks Rûdolfs
+
+#10)Ratnieks Paulis
+
+#11)Rimkçvièa Alise
+
+#12)Samsonova Anna
+
+#13)Zîverte Mâra Anna
+
