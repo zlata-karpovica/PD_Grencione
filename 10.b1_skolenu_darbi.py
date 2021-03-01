2e9218515d180685a67e47d679ea784df3eb8a91
Grencione <76771267+Grencione@users.noreply.github.com>
2021-03-01 23:14:48 +0200

    Add files via upload

---
+++ 10.b1_skolenu_darbi.py
@@ -0,0 +1,27 @@
+Tatjana Grencione
+
+1 Akimovs Sergejs
+
+2 Druzs Dmitrijs
+
+3 Fedaks Alekss
+
+4 Gaile Aïona
+
+5 Gaile Anna
+
+6 Griðaèova Violeta
+
+7 Jakovïevs Aleksandrs
+
+8 Jeroðèenkova Alîna
+
+9 Kopilovs Marks
+
+10 Kostenko Karîna
+
+11 Kuzòecovs Ivans
+
+12 Ïetko Jevgçnijs
+
+13 Skuridins Artis Daniels
