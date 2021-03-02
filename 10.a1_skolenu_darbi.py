bc2d43a071c24e9d4fac55cf752d7258d9e95816
Grencione <76771267+Grencione@users.noreply.github.com>
2021-03-02 23:06:26 +0200

    Add files via upload

---
+++ 10.a1_skolenu_darbi.py
@@ -0,0 +1,23 @@
+1 Arhipenko Sofja
+
+2 Berga Laura
+
+3 Fiïimonovs Eldars
+
+4 Gaiðute Ksenija
+
+5 Gorniks Arturs
+
+6 Guþonoka Jevgeòija
+
+7 Isaèenko Aleksandrs
+
+8 Komaðilo Dmitrijs
+
+9 Lukjanèikova Darja
+
+10 Minalto Jana
+
+11 Obrazcova Marija
+
+12 Ðvacka Marija
---
+++ 10.a2_skolenu_darbi.py
@@ -0,0 +1,25 @@
+1 Apsîtis Andris
+
+2 Bçrziòð Oïegs
+
+3 Èerniðova Sofija
+
+4 Gorjuðkins Iïja
+
+5 Lanka Elîna
+
+6 Lazarevs Artjoms
+
+7 Ïebede Anastasija
+
+8 Maïinovska Karîna
+
+9 Mieme Karîna
+
+10 Orlova Sofija
+
+11 Stukone Patrîcija
+
+12 Tûcis Òikita
+
+13 Zakrevskis Òikita
