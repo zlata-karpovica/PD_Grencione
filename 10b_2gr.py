ffcc551c45328a317e64509dd26a982d84fd0bfe
Grencione <76771267+Grencione@users.noreply.github.com>
2021-03-04 09:43:36 +0200

    Add files via upload

---
+++ 10b_2gr.py
@@ -0,0 +1,26 @@
+1 	
+Kudrjavceva Veronika
+2 	
+Melnic Ana
+3 	
+Mihailovs Alekss
+4 	
+Orlova Katerina
+5 	
+Rimare Sabîne
+6 	
+Sidorenko Aleksandrs
+7 	
+Siïiòð Deniss
+8 	
+Skodorovs Vlads
+9 	
+Smirnovs Volodars
+10 	
+Verþanovskis Marks
+11 	
+Volkova Veronika
+12 	
+Zariòð Roberts
+13 	
+Þaravins Kirills
